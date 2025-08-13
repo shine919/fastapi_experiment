22ad4985f3d4763f20ac9b66465e59f855e2973f
shine919 <overuber@hotmail.com>
2025-08-13 17:56:30 +0400

    create user model

---
+++ app/models/user.py
@@ -0,0 +1,26 @@
+from enum import Enum
+from typing import List, TYPE_CHECKING
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.types import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+
+
+from .base import Base
+class UserRoleEnum(str,Enum):
+    admin = 'admin'
+    user = 'user'
+
+
+class User(Base):
+
+    id:Mapped[int] = mapped_column(primary_key=True)
+    username:Mapped[str] = mapped_column(String(50),nullable=False)
+    password:Mapped[str] = mapped_column(nullable=False)
+    email:Mapped[str] = mapped_column(String(50),nullable=False)
+    role:Mapped[UserRoleEnum]=mapped_column(default=UserRoleEnum.user,server_default=UserRoleEnum.user,name='user_role_enum')
+
+    def __repr__(self):
+        return f"id={self.id}, username={self.username}, email={self.email}"
