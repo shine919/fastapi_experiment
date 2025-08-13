39572f5cc79fd8bc01f2fc450fe5977cd258f318
shine919 <overuber@hotmail.com>
2025-08-13 17:56:01 +0400

    create schemas user

---
+++ app/user/schema.py
@@ -0,0 +1,42 @@
+from fastapi import Body
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+
+class UserBase(BaseModel):
+    username: str
+
+class Roles(BaseModel):
+    role_name:str
+
+class UserLogin(UserBase):
+    password: str
+
+class User(UserBase):
+    email: EmailStr | None = None
+
+class UserRegister(User,UserLogin):
+    pass
+class UserFromDB(User):
+    disabled: bool = False
+    roles: list[str]
+
+class UserCheck(BaseModel):
+    id:  int
+    username: str
+    password:str
+    email:str
+    role: str
+    model_config=ConfigDict(from_attributes=True)
+
+class UserPatch(BaseModel):
+    id:int
+    username: str | None = None
+    password: str | None = None
+    email: str | None = None
+class UserPut(UserCheck):
+    pass
+
+class UsersFromDBList(BaseModel):
+    users: list[UserCheck]
+class UserToDB(UserFromDB):
+    password :str
