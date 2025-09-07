2c9333ccf0ad3dce8739d9150816831ab9ff9539
starflow9 <overuber@hotmail.com>
2025-09-07 22:52:35 +0400

    delete user_data

--- app/db.py
+++ app/db.py
@@ -36,25 +36,6 @@ async def get_db():
         client.close()
 
 
-# Фиктивные данные пользователей (в реальном проекте тут будет БД)
-USERS_DATA = [
-    {
-        "username": "admin",
-        "password": "$2b$12$0GvkzMFS0uoFutFZhcfYVObbWyat3Up5aWnZsCZIbxVtqGqECAqga",  # В продакшене пароли должны быть хешированы!
-        "roles": ["admin"],
-        "full_name": "Admin User",
-        "email": "admin@example.com",
-        "disabled": False,
-    },
-    {
-        "username": "user",
-        "password": "$2b$12$aBWf7Ws6gmjIg9dMwwY7s.Vne9GrSogc5hR8JU1DG2YHX.pWsdOJW",
-        "roles": ["user"],
-        "full_name": "Regular User",
-        "email": "user@example.com",
-        "disabled": False,
-    },
-]
 resources = {
     "alice": {"content": "Секретные данные Алисы", "is_public": False},
     "user": {"content": "Секретные данные Usera", "is_public": False},
