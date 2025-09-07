f7d70efed15e8dea796dec4a277b19932e24a3c7
starflow9 <overuber@hotmail.com>
2025-09-07 22:53:57 +0400

    include docs router

--- app/router.py
+++ app/router.py
@@ -1,3 +1,4 @@
+from docs import router as docs_router
 from fastapi import APIRouter
 from todo.views import router as todo_router
 from user.views import router as user_router
@@ -6,3 +7,5 @@ router = APIRouter()
 
 router.include_router(todo_router)
 router.include_router(user_router)
+
+router.include_router(docs_router)
