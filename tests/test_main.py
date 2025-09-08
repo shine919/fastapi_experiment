9d6dfc09f03558d5b64dccc70f266c2a9ebb3fd6
starflow9 <overuber@hotmail.com>
2025-09-08 18:47:30 +0400

    sleep

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,25 +1,19 @@
-import requests
-from main import fetch_data
+# class TestMock:
+#     async def test_simple(self, client):
+#         print("Тест выполняется!")
+#         resp = await client.get("users/user/2/")
+#         print(f"Статус: {resp.status_code}")
+#         print(f"Ответ: {resp.json()}")
+#
+#     async def test_get_user(self, mocker, client):
+#         mock_usr_data = {"user": "admin", "id": 1}
+#         mock_f_data = requests.Timeout()
+#         mock_check_user = mocker.patch("user.crud.UserOrm.check_user_orm", return_value=mock_usr_data)
+#         mock_fetch_data = mocker.patch("main.get_rekt", return_value=mock_f_data)
+#
+#         resp = await client.get("/users/user/1/")
+#         mock_fetch_data.assert_called_once()
+#         mock_check_user.assert_called_once()
+#
+#         assert resp.json() == mock_usr_data
 
-
-class TestMock:
-    async def test_simple(self, client):
-        print("Тест выполняется!")
-        resp = await client.get("users/user/2/")
-        print(f"Статус: {resp.status_code}")
-        print(f"Ответ: {resp.json()}")
-
-    async def test_get_user(self, mocker, client):
-        mock_usr_data = {"user": "admin", "id": 1}
-        mock_f_data = requests.Timeout()
-        mock_check_user = mocker.patch("user.crud.UserOrm.check_user_orm", return_value=mock_usr_data)
-        mock_fetch_data = mocker.patch("main.get_rekt", return_value=mock_f_data)
-
-        resp = await client.get("/users/user/1/")
-        resp_2 = await fetch_data()
-        assert isinstance(resp_2, Exception)
-        mock_fetch_data.assert_called_once()
-        mock_check_user.assert_called_once()
-
-        assert resp.json() == mock_usr_data
-        assert resp_2 == mock_f_data
