3868ebb359dd134f91d0e64440db85e999b35684
starflow9 <overuber@hotmail.com>
2025-09-08 18:42:09 +0400

    return

--- app/core/logger.py
+++ app/core/logger.py
@@ -3,9 +3,10 @@ import logging
 from core.config import settings
 
 
-def get_logger(level: str = settings.level):
+def get_logger(level: str = settings.loger.level):
     logging.basicConfig(
         level=level,
         datefmt="%Y-%m-%d %H:%M:%S",
-        format="[%(asctime)s.%(msec)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",
+        format="%(asctime)s.%(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",
     )
+    return logging
